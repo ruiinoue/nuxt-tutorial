a50e71334ad90c68a120c29bd4fa735434a0229e
ruiinoue <jingshangrui422@gmail.com>
2021-01-15 23:19:34 +0900

    非同期通信を含むコンテンツのサーバーサイドレンダリング

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    <!--  -->
-  </div>
+  <section class="container">
+    <div>
+      <h3>Nuxt.js のタグが付けられた投稿の一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4>
+            <span>{{item.title}}</span>
+            <span> by {{item.user.id}}</span>
+          </h4>
+          <div>{{item.body.slice(0, 130)}}</div>
+          <p><a :href="item.url">{{item.url}}</a></p>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  async mounted() {
-    console.log(
-      JSON.stringify(
-        await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '')
-    )
+  // async mounted() {
+  //   console.log(
+  //     JSON.stringify(
+  //       await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '')
+  //   )
+  // },
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+    return {
+      items
+    }
   },
 }
 </script>
@@ -18,31 +36,21 @@ export default {
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  padding: 16px;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+h3 {
+  margin: 16px 0;
+  padding: 8px 0;
+  border-bottom: solid 1px #e5e5e5;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+li + li {
+  margin: 16px 0;
 }
 
-.links {
-  padding-top: 15px;
+p {
+  margin: 8px 0;
 }
 </style>
 
