f5a54d687bf5174244f5d1cb81efc153dd0b0ff5
ruiinoue <jingshangrui422@gmail.com>
2021-01-15 22:00:22 +0900

    axios-moduleによる外部リソースの取得

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,17 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        Hello, World
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <!--  -->
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
+  async mounted() {
+    console.log(
+      JSON.stringify(
+        await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, '')
+    )
+  },
 }
 </script>
 
