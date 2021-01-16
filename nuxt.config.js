9cd68aee3a8942ba4b92f728cc25f65dce9963ae
ruiinoue <60529151+ruiinoue@users.noreply.github.com>
2021-01-16 11:00:13 +0900

    HTMLメタの設定 (#10)

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ module.exports = {
   */
   head: {
     title: 'nuxt-tutorial',
+    titleTemplate: '%s | Nuxt.js tag items viewer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
