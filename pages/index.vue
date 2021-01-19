14181793996a1f1aa7d75cd401d42afcb2704255
ruiinoue <60529151+ruiinoue@users.noreply.github.com>
2021-01-19 12:11:50 +0900

    簡単なmiddlewareの実装 (#17)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
     <h1>Index Page</h1>
+    <ul>
+      <li>
+        <nuxt-link to="/login">ログインページへ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/authed-route">認証が必要なぺージへ</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
