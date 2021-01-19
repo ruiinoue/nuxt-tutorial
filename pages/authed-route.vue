79a17e18f3c1f95b97fea75f8be5380dbfb222ad
ruiinoue <jingshangrui422@gmail.com>
2021-01-19 12:00:07 +0900

    簡単なmiddlewareの実装

---
+++ pages/authed-route.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>認証が必要なページ</h2>
+    <p>
+      Cookie の 'credential' 値が Truthy な場合のみアクセス可能です。
+      <nuxt-link to="/">トップぺージに戻る</nuxt-link>
+    </p>
+  </div>
+</template>
